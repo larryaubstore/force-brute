0ecd02c8db9a4bff0fe63cf66ae53e9ca40b6fca
Laurence Morin-Daoust <laurence_morin_daoust@gmail.com>
2012-05-27 20:52:25 -0400

    Picking added.

--- javascript/forcebrute.js
+++ javascript/forcebrute.js
@@ -8,15 +8,22 @@ var FORCEBRUTE = (function () {
 
     var myMovingElement, myImagesList, counter, preloadImages,
         preloadImagesCallback, myImagesLoaded, renderingLoop,
-        boatYPosition, imgFront, imgGrid, myGrid, myImagesListGrid;
+        boatYPosition, imgFront, imgGrid, myGrid, myImagesListGrid,
+        myRedSquare, imgRedSquare, bindEvents,
+        redSquareXPos, redSquareYPos;
 
     myMovingElement = ["../images/bateau/0001-min.png"];
-    myGrid = ["../images/grid/v3/0001.png"];
+    myGrid = ["../images/grid/v3/0002.png"];
+    myRedSquare = ["../images/redsquare/0003.png"];
 
     myImagesLoaded = [];
     myImagesList = [];
     myImagesListGrid = [];
     boatYPosition = 500;
+
+    // Position du carré rouge
+    redSquareXPos = 0;
+    redSquareYPos = 0;
    
     // Charger un groupe d'images
     // 0001.jpg, 0002, jpg 
@@ -41,6 +48,31 @@ var FORCEBRUTE = (function () {
 
     };
 
+    bindEvents = function () {
+        $("#testbutton").bind('click', function () {
+            redSquareXPos += 18.0;
+            redSquareYPos -= 24.0;
+        });
+
+        var canvas = document.getElementById('canvas');
+        
+        canvas.onclick = function (evt) {
+            var top, left, mouseX, mouseY, xPos, yPos, rawX, rawY;
+    
+            top = 0;
+            left = 0;
+            
+            mouseX = evt.clientX - left + window.pageXOffset;
+            mouseY = evt.clientY - top + window.pageYOffset;
+
+            rawX = parseInt((mouseX - 6) / 18, 10);
+            rawY = 30 - parseInt(mouseY / 24, 10);
+
+            redSquareXPos = rawX * 18;
+            redSquareYPos = rawY * 24 * -1;
+        };
+    };
+
     preloadImages = function () {
         
         var img, counter = 0;
@@ -51,6 +83,11 @@ var FORCEBRUTE = (function () {
         imgGrid = new Image();
         imgGrid.src = myGrid[0];
 
+        imgRedSquare = new Image();
+        imgRedSquare.src = myRedSquare[0];
+
+        
+
         for (counter = 1; counter <= myImagesList.length; counter += 1) {
             img = new Image();
             img.onload = preloadImagesCallback(counter - 1, img);
@@ -59,7 +96,8 @@ var FORCEBRUTE = (function () {
     };
     renderingLoop = function (imageIndex) {
 
-        var canvas = document.getElementById('canvas'), ctx, img, newIndex, testGridImage;
+        var canvas = document.getElementById('canvas'), ctx, img,
+            newIndex, testGridImage;
         ctx = canvas.getContext('2d');
         //ctx.globalAlpha = 0;
         
@@ -71,6 +109,8 @@ var FORCEBRUTE = (function () {
             ctx.drawImage(img, 0, 0);
             // Dessiner la grille blanche
             ctx.drawImage(imgGrid, 0, 0);
+            // Dessiner les carrés rouges
+            ctx.drawImage(imgRedSquare, redSquareXPos, redSquareYPos);
             // Dessiner un bateau
             ctx.drawImage(imgFront, 500, boatYPosition);
         } catch (exception) {
@@ -87,7 +127,8 @@ var FORCEBRUTE = (function () {
         RenderImages: function () {
             preloadImages();
         },
-        RenderingLoop: renderingLoop
+        RenderingLoop: renderingLoop,
+        BindEvents: bindEvents
     };
 }());
 
