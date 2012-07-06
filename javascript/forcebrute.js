1ff1fdc6c46dbcbb8431876820db21b0368d85d3
Laurence Morin-Daoust <laurence.morin.daoust@gmail.com>
2012-07-05 23:04:53 -0400

    Explosion added.

--- javascript/forcebrute.js
+++ javascript/forcebrute.js
@@ -11,7 +11,8 @@ var FORCEBRUTE = (function () {
         boatYPosition, imgFront, imgGrid, myGrid, myImagesListGrid,
         myRedSquare, imgRedSquare, bindEvents,
         redSquareXPos, redSquareYPos, myArrow, imgArrow,
-        myGridVisible, myImagesLoadedGrid;
+        myGridVisible, myImagesLoadedGrid, myImagesBoatExplosion,
+        imgBoatExplosion, stopExplosion, resetExplosion;
 
     myMovingElement = ["../images/bateau/0001-min.png"];
     myGrid = ["../images/grid/v3/0002.png"];
@@ -19,11 +20,14 @@ var FORCEBRUTE = (function () {
     myArrow = ["../images/arrow/0001.png"];
 
     myGridVisible = false;
+    stopExplosion = false;
+    resetExplosion = false;
 
     myImagesLoaded = [];
     myImagesLoadedGrid = [];
     myImagesList = [];
     myImagesListGrid = [];
+    myImagesBoatExplosion = [];
     boatYPosition = 500;
 
     // Position du carré rouge
@@ -35,6 +39,7 @@ var FORCEBRUTE = (function () {
     for (counter = 1; counter < 10; counter += 1) {
         myImagesList.push("../images/water/v2/000" + counter + ".jpg");
         myImagesListGrid.push("../images/grid/v7/000" + counter + ".png");
+        myImagesBoatExplosion.push("../images/explosion/testCollision/000" + counter + ".png");
     }
 
     // Charger un groupe d'images
@@ -42,12 +47,14 @@ var FORCEBRUTE = (function () {
     for (counter = 10; counter <= 30; counter += 1) {
         myImagesList.push("../images/water/v2/00" + counter + ".jpg");
         myImagesListGrid.push("../images/grid/v7/00" + counter + ".png");
+        myImagesBoatExplosion.push("../images/explosion/testCollision/00" + counter + ".png");
     }
 
 
-    preloadImagesCallback = function (imageIndex, img, imgGrid) {
+    preloadImagesCallback = function (imageIndex, img, imgGrid, imgBoat) {
         myImagesLoaded[imageIndex.toString()] = img;
         myImagesLoadedGrid[imageIndex.toString()] = imgGrid;
+        myImagesBoatExplosion[imageIndex.toString()] = imgBoat;
 
         if (myImagesLoaded.length === 30) {
             // Loading complete
@@ -63,8 +70,17 @@ var FORCEBRUTE = (function () {
         });
 
         $("#showGridButton").bind('click', function () {
-            myGridVisible = false;
+	    // Invert status
+            myGridVisible = !myGridVisible;
         });
+
+        $("#explosionButton").bind('click', function () {
+
+		stopExplosion = false;
+		newIndex = 0;
+		//imageIndex = 0;
+        });
+
 
         var canvas = document.getElementById('canvas');
         
@@ -87,7 +103,7 @@ var FORCEBRUTE = (function () {
 
     preloadImages = function () {
         
-        var img, imgGridBatch, counter = 0;
+        var img, imgGridBatch, imgBoatExplosion, counter = 0;
 
         imgFront = new Image();
         imgFront.src = myMovingElement[0];
@@ -109,13 +125,16 @@ var FORCEBRUTE = (function () {
             imgGridBatch = new Image();
             imgGridBatch.src = myImagesListGrid[counter - 1];
 
-            img.onload = preloadImagesCallback(counter - 1, img, imgGridBatch);
+            imgBoatExplosion = new Image();
+            imgBoatExplosion.src = myImagesBoatExplosion[counter - 1];
+
+            img.onload = preloadImagesCallback(counter - 1, img, imgGridBatch, imgBoatExplosion);
         }
     };
     renderingLoop = function (imageIndex) {
 
         var canvas = document.getElementById('canvas'), ctx, img,
-            newIndex, testGridImage;
+            newIndex, testGridImage, imgBoatIntact;
         ctx = canvas.getContext('2d');
         //ctx.globalAlpha = 0;
         
@@ -123,17 +142,32 @@ var FORCEBRUTE = (function () {
         img = myImagesLoaded[imageIndex];
         testGridImage = myImagesLoadedGrid[imageIndex];
 
+	if(stopExplosion == false) {
+		
+	    imgBoatExplosion = myImagesBoatExplosion[imageIndex];
+
+	    //debugger;
+	    if(imageIndex >= 28)  {
+		stopExplosion = true;
+	    }
+	}
+        imgBoatIntact = myImagesBoatExplosion[1];
+
         try {
             // Dessiner le plan d'eau
             ctx.drawImage(img, 0, 0);
 
             // Dessiner la grille flottante
-            ctx.drawImage(testGridImage, 0, 0);
+            //ctx.drawImage(testGridImage, 0, 0);
 
             if (myGridVisible === true) {
                 // Dessiner la grille blanche
                 ctx.drawImage(imgGrid, 0, 0);
             }
+
+            // Dessiner le bateau qui explose
+            ctx.drawImage(imgBoatExplosion, 200, 200);
+            //ctx.drawImage(imgBoatIntact, 200, 200);
             // Dessiner les carrés rouges
             ctx.drawImage(imgRedSquare, redSquareXPos, redSquareYPos);
             // Dessiner un bateau
@@ -146,7 +180,12 @@ var FORCEBRUTE = (function () {
         // Faire avancer le bateau
         boatYPosition -= 1;
 
-        newIndex = (imageIndex + 1) % (myImagesLoaded.length - 1);
+	if(resetExplosion == true) {
+	    newIndex = 0;
+	}
+	else {		
+            newIndex = (imageIndex + 1) % (myImagesLoaded.length - 1);
+	}
         setTimeout('FORCEBRUTE.RenderingLoop(' + newIndex + ')', 50);
     };
 
